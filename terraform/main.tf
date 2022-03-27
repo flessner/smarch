c89204b6571e89d01dfbddc8d5e67e62eacb9783
flessner <torben@flessner.io>
2022-03-28 01:37:02 +0200

    removing useless tf output

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,15 +16,3 @@ terraform {
 provider "digitalocean" {
   token = var.DO_PAT
 }
-
-output "url_web" {
-  value = digitalocean_app.web_main.default_ingress
-}
-
-output "url_cloud" {
-  value = digitalocean_app.web_cloud.default_ingress
-}
-
-output "url_api" {
-  value = digitalocean_app.api_nyc.default_ingress
-}
