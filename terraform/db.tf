01b7a8d52095b7ecd59a55608d493f3ca6354bde
flessner <torben@flessner.io>
2022-06-20 01:04:41 +0200

    remove kubernetes

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,18 +1,3 @@
-resource "kubernetes_persistent_volume_claim" "test-pv" {
-  metadata {
-    name = "test-pv"
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "do-block-storage"
-    resources {
-      requests = {
-        storage = "200Gi"
-      }
-    }
-  }
-}
-
 resource "digitalocean_database_cluster" "redis_nyc" {
   name       = "smarch-redis"
   engine     = "redis"
--- terraform/main.tf
+++ terraform/main.tf
@@ -22,34 +22,3 @@ provider "digitalocean" {
 provider "cloudflare" {
   api_token = var.CF_TOKEN
 }
-
-// KUBERNETES
-resource "digitalocean_kubernetes_cluster" "cluster_kiwi" {
-  name    = "smarch-kiwi"
-  region  = "fra1"
-  version = "1.22.8-do.1"
-
-  node_pool {
-    name       = "worker-pool"
-    size       = "s-2vcpu-2gb-amd"
-    node_count = 3
-  }
-}
-
-provider "kubernetes" {
-  host             = digitalocean_kubernetes_cluster.cluster_kiwi.endpoint
-  token            = digitalocean_kubernetes_cluster.cluster_kiwi.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.cluster_kiwi.kube_config[0].cluster_ca_certificate
-  )
-}
-
-provider "helm" {
-  kubernetes {
-    host = digitalocean_kubernetes_cluster.cluster_kiwi.endpoint
-    cluster_ca_certificate = base64decode(
-      digitalocean_kubernetes_cluster.cluster_kiwi.kube_config[0].cluster_ca_certificate
-    )
-    token = digitalocean_kubernetes_cluster.cluster_kiwi.kube_config[0].token
-  }
-}
