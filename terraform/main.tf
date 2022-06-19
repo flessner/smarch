4c9ba78f4ea58e73aa9c7a835faecf8f71916c2b
flessner <torben@flessner.io>
2022-06-19 18:19:33 +0200

    removing project

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,16 +21,3 @@ provider "digitalocean" {
 provider "cloudflare" {
   api_token = var.CF_TOKEN
 }
-
-resource "digitalocean_project" "project_smarch" {
-  name        = "smarch"
-  resources = [
-    digitalocean_app.web_cloud.id,
-    digitalocean_app.web_main.id,
-    digitalocean_app.web_terminal.id
-  ]
-}
-
-output "web-id" {
-  value = digitalocean_app.web_cloud.id
-}
