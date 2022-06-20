402dc7b5082b328af4a991f55b4061edb071bbce
flessner <torben@flessner.io>
2022-06-20 18:45:27 +0200

    updating commands

--- terraform/db.tf
+++ terraform/db.tf
@@ -16,7 +16,8 @@ resource "digitalocean_droplet" "cb" {
 
   provisioner "remote-exec" {
     inline = [
-      "docker run -d --name db-p 8091-8096:8091-8096 -p 11210-11211:11210-11211 -v ~/mnt/smarch_cb:/opt/couchbase/var couchbase"
+      "sleep 10s",
+      "docker run -d --name db -p 8091-8096:8091-8096 -p 11210-11211:11210-11211 -v /mnt/smarch_cb:/opt/couchbase/var couchbase"
     ]
   }
 }
