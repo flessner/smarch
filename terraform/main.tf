5f35e6fe2930b0a96ba2852eea5ff946ee195d3e
flessner <torben@flessner.io>
2022-06-20 13:47:34 +0200

    removing cloudflare

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,22 @@ variable "CF_TOKEN" {}
 
 // SETUP
 terraform {
+  cloud {
+    organization = "smarch"
+
+    workspaces {
+      name = "smarch"
+    }
+  }
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
-    }
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "~> 3.0"
     }
   }
 }
 
 provider "digitalocean" {
   token = var.DO_PAT
-}
-
-provider "cloudflare" {
-  api_token = var.CF_TOKEN
 }
