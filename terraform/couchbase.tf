eef3a02933068f2935d9709e08f2177bd46bc918
flessner <torben@flessner.io>
2022-06-20 22:26:16 +0200

    update cb master node

--- terraform/couchbase.tf
+++ terraform/couchbase.tf
@@ -6,19 +6,19 @@ resource "digitalocean_droplet" "cb-master" {
   volume_ids = [ digitalocean_volume.cb-master.id ]
   ssh_keys = [ digitalocean_ssh_key.default.id ]
 
+  connection {
+    type = "ssh"
+    host = self.ipv4_address
+    user = "root"
+    timeout = "300"
+    private_key = tls_private_key.default.private_key_pem
+  }
+
   provisioner "remote-exec" {
     inline = [
       "sleep 10s",
       "docker run -d --name db -p 8091-8096:8091-8096 -p 11210-11211:11210-11211 -v /mnt/smarch_cb:/opt/couchbase/var couchbase"
     ]
-
-    connection {
-      type = "ssh"
-      host = self.ipv4_address
-      user = "root"
-      timeout = "300"
-      private_key = tls_private_key.default.private_key_pem
-    }
   }
 }
 
