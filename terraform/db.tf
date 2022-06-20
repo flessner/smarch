e98ba0e1f28488ddd54ed21fcfbe53532e2081f6
flessner <torben@flessner.io>
2022-06-20 14:23:46 +0200

    removing redis

--- terraform/db.tf
+++
@@ -1,8 +0,0 @@
-resource "digitalocean_database_cluster" "redis_nyc" {
-  name       = "smarch-redis"
-  engine     = "redis"
-  version    = 6
-  size       = "db-m-4vcpu-32gb"
-  region     = "fra1"
-  node_count = 1
-}
